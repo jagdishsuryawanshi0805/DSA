caa6e7b56d0dd648860b33b986efa381025c9b15
Jagdish Suryawanshi <jagdishsuryawanshi0805@gmail.com>
2025-07-25 22:14:47 +0530

    Practice

--- arrays/Remove Element Two Pointer.py
+++ arrays/Remove Element Two Pointer.py
@@ -1,8 +1,13 @@
 class Solution:
-    def removeElement(self, nums: list[int], val: int) -> int:
-        k = 0  # pointer for the next position of non-val element
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]  # move the non-val element forward
-                k += 1
-        return k  # new length of modified array
+    def removeElement(self, nums, val):
+        i = 0
+        n = len(nums)
+
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n -= 1
+            else:
+                i += 1
+
+        return n
